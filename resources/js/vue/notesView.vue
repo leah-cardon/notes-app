6a9469acbd4bc3d1454f185c57ae9d03f1ffb4b6
Leah Cardon <cardonleah@gmail.com>
2021-02-07 15:58:30 -0700

    Added notes infrastructure to notesView

--- resources/js/vue/notesView.vue
+++ resources/js/vue/notesView.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-    Notes View
+    <div f-for="(item, index) in items" :key="index" >
+      <list-item
+        :item="item"
+        class="item"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import noteItem from './noteItem'
 
+export default {
+  props: ['items'],
+  components: {
+    noteItem
+  }
 }
 </script>
+
+<style scoped>
+.item {
+  background: #e6e6e6;
+  padding: 5px;
+  margin-top: 5px;
+}
