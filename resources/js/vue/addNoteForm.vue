def14092e1ad797ed8ae7bbe050ac9a2bf4a7235
Leah Cardon <cardonleah@gmail.com>
2021-02-07 15:13:47 -0700

    Styled form input

--- resources/js/vue/addNoteForm.vue
+++ resources/js/vue/addNoteForm.vue
@@ -12,3 +12,19 @@ export default {
 
 }
 </script>
+
+<style scoped>
+.addItem {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+input {
+  background: #f7f7f7;
+  border: 0px;
+  outline: none;
+  padding: 5px;
+  margin-right: 10px;
+  width: 100%;
+}
+</style>
