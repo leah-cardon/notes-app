9742de456ee56dbd8d175aab6ed0591966cbb199
Leah Cardon <cardonleah@gmail.com>
2021-02-07 17:11:35 -0700

    Added edit button to list items

--- resources/js/vue/noteItem.vue
+++ resources/js/vue/noteItem.vue
@@ -1,11 +1,62 @@
 <template>
   <div class="item">
-    <span>{{ item.name }}</span>
+    <div class="titleText">{{ item.name }}</div>
+    <div class="noteText">{{ item.content }}</div>
+    <!-- CHANGE THIS:
+    edit button opens the text for editing
+    a save button sends the updateNote (put request) -->
+    <button @click="updateNote()" class="edit">
+      <font-awesome-icon icon="edit" />
+    </button>
+    <button @click="deleteNote()" class="trashcan">
+      <font-awesome-icon icon="trash" />
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['item']
+  props: ['item'],
+  methods: {
+    updateNote() {
+      axios.put('api/item/' + this.item.id, {
+        item: this.item
+      })
+      .then(response => {
+        if (response.status == 200) {
+          this.$emit('itemchanged');
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }
+  }
 }
 </script>
+
+<style scoped>
+.titleText {
+  font-size: 40px;
+}
+.noteText {
+  font-size: 12px;
+}
+.item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.edit {
+  background: #e6e6e6;
+  border: none;
+  color: black;
+  outline: none;
+}
+.trashcan {
+  background: #e6e6e6;
+  border: none;
+  color: #FF0000;
+  outline: none;
+}
+</style>
